0e3d0564035786eb2997061e7d4a92c184f982a6
Conor Nosal <c.nosal@gmail.com>
2019-12-06 23:56:10 -0500

    strip symbol table and debug info from final builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ pushd `dirname $0`/.. > /dev/null
 if [[ "$1" == "all" ]]
 then
   go fmt ./... && \
-    GOOS=linux go build -o ./manifer_linux -i ./cmd/manifer && \
-    GOOS=darwin go build -o ./manifer_darwin -i ./cmd/manifer && \
-    GOOS=windows go build -o ./manifer_windows.exe -i ./cmd/manifer
+    GOOS=linux go build -ldflags="-s -w" -o ./manifer_linux -i ./cmd/manifer && \
+    GOOS=darwin go build -ldflags="-s -w" -o ./manifer_darwin -i ./cmd/manifer && \
+    GOOS=windows go build -ldflags="-s -w" -o ./manifer_windows.exe -i ./cmd/manifer
 else
   go fmt ./... && \
     go build -o ./manifer -i ./cmd/manifer
